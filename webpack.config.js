387e4e36212f229a068390ea29f55c644de263de
git config --global user.name <RamPG>
2020-05-29 23:03:19 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require(`path`);
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
