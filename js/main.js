a31f1f8437592d90909189135deac250c7a48782
git config --global user.name <vlasovnew@yandex.ru>
2020-05-31 13:32:54 +0300

    remove babel, add online showStyles, rework get and post requests

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import modals from "./modules/modals";
 import slider from "./modules/slider";
 import showStyles from "./modules/showStyles";
-// import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     slider();
-    showStyles();
+    showStyles(".button-transparent", ".hidden-lg", "#styles > div > div");
     // forms("form");
 });
--- js/modules/showStyles.js
+++ js/modules/showStyles.js
@@ -1,13 +1,35 @@
-const showStyles = () => {
-    const button = document.querySelector(".button-transparent");
-    const otherStyles = document.querySelectorAll(".hidden-lg");
-    button.addEventListener("click", () => {
-        button.style.display = "none";
+import {getData} from "../services/requests";
+
+const showStyles = (buttonSelector, otherStylesSelector, wrapper) => {
+    const button = document.querySelector(buttonSelector);
+    const otherStyles = document.querySelectorAll(otherStylesSelector);
+    const bindStylesOffline = () => {
         otherStyles.forEach((item) => {
             item.classList.add('animated', 'fadeInUp');
             item.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
             item.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
         })
+    };
+    const bindStyleOnline = (data) => {
+        data.forEach((item) => {
+            const element = document.createElement("div");
+            element.classList.add('animated', 'fadeInUp');
+            element.innerHTML = `
+                <div class="styles-block">
+                    <img src=${item.src} alt="style">
+                    <h4>${item.title}</h4>
+                    <a href=${item.link}>Подробнее</a>
+                </div>
+            `;
+            element.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+            document.querySelector(wrapper).append(element);
+        })
+    };
+    button.addEventListener("click", () => {
+        button.style.display = "none";
+        getData("assets/db.json")
+            .then(res => bindStyleOnline(res.styles))
+            .catch(bindStylesOffline);
     });
 }
 
